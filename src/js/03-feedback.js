79b792a4e435c27c774827025bc97373aae9974c
Dima-Shvets <81581083+Dima-Shvets@users.noreply.github.com>
2021-11-01 20:23:00 +0200

    adds feedback form feature

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,21 +2,34 @@ import throttle from 'lodash.throttle'
 
 import refs from '../refferences/refs'
 
-refs.feedbackForm.addEventListener('input', throttle(onFormInputChange, 500))
 
-function onFormInputChange(e) {
-    const inputsValues = {
+if (localStorage.getItem('feedback-form-state')) {
+    refs.feedbackForm.elements.email.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+}
+
+if (localStorage.getItem('feedback-form-state')) {
+    refs.feedbackForm.elements.message.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+}
+
+refs.feedbackForm.addEventListener('input', throttle(onFormInputChange, 500));
+refs.feedbackForm.addEventListener('submit', () => {localStorage.removeItem('feedback-form-state')})
+
+let inputsValues = {
         email: '',
         message: '',
     }
+
+function onFormInputChange(e) {
+   
     if (e.currentTarget.elements.email.value) {
         inputsValues.email = e.currentTarget.elements.email.value;
+    }
+
+    if (e.currentTarget.elements.message.value) {
+        inputsValues.message = e.currentTarget.elements.message.value;
     }
 
     localStorage.setItem('feedback-form-state', JSON.stringify(inputsValues))
 }
 
-const s = {
-    message: 'dkkdd',
-}
-localStorage.setItem('feedback-form-state', JSON.stringify(s))
+
